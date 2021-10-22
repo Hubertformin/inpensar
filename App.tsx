db3bf6bcb282ccb1be811cc9083bc424a0f1aa33
Hubertformin <Rosemary.com98>
2021-10-22 15:59:09 +0100

    Initialize app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+import useColorScheme from './hooks/colorScheme';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Navigation from './navigation';
+import { View } from './components/Themed';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const colorScheme = useColorScheme();
+  const [appLoaded, setAppLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    //initialize app then hide splashscreen
+    initApp();
+  }, [])
+
+  async function initApp() {
+    let customFonts = {
+      'Gilroy': require('./assets/fonts/gilroy/Gilroy-Regular.ttf'),
+      'Gilroy-medium': require('./assets/fonts/gilroy/Gilroy-Medium.ttf'),
+      'Gilroy-semibold': require('./assets/fonts/gilroy/Gilroy-Semibold.ttf'),
+      'Gilroy-bold': require('./assets/fonts/gilroy/Gilroy-Bold.ttf'),
+    };
+    // load fonts
+    await Font.loadAsync(customFonts);
+    setAppLoaded(true);
+  }
+
+  if (appLoaded) {
+    return (
+      <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+    );
+  } else {
+    return (<View></View>);
+  }
+}
